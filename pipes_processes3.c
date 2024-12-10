88d3201d2d7c828df13964f1d09306a31f90941d
prighimire <131629703+prighimire@users.noreply.github.com>
2024-12-09 20:12:09 -0500

    initial commit

--- pipes_processes3.c
+++ pipes_processes3.c
@@ -3,47 +3,84 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  int pipefd1[2]; 
-  int pipefd2[2];
-  int pid1, pid2;
-
-  char *cat_args[] = {"cat", "scores", NULL};
-  char *grep_args[] = {"grep", argv[1], NULL}; 
-  char *sort_args[] = {"sort", NULL};
-
-  // Create two pipes
-  pipe(pipefd1); 
-  pipe(pipefd2);
-
-  pid1 = fork();
-  if (pid1 == 0) {
-    // Child process 1 (grep)
-    dup2(pipefd1[0], STDIN_FILENO);  // Replace stdin with read end of pipe 1
-    dup2(pipefd2[1], STDOUT_FILENO); // Replace stdout with write end of pipe 2
-    close(pipefd1[1]);  // Close unused write end of pipe 1 
-    close(pipefd2[0]);  // Close unused read end of pipe 2
-    execvp("grep", grep_args); // Execute grep
-  } 
-  else {
-    pid2 = fork();
-    if (pid2 == 0) {
-      // Child process 2 (sort)  
-      dup2(pipefd2[0], STDIN_FILENO);  // Replace stdin with read end of pipe 2
-      close(pipefd1[0]);  // Close unused read end of pipe 1
-      close(pipefd1[1]);  // Close unused write end of pipe 1
-      close(pipefd2[1]);  // Close unused write end of pipe 2
-      execvp("sort", sort_args); // Execute sort
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <grep-argument>\n", argv[0]);
+        exit(1);
     }
-    else {  
-      // Parent process (cat)
-      dup2(pipefd1[1], STDOUT_FILENO); // Replace stdout with write end of pipe 1
-      close(pipefd1[0]);  // Close unused read end of pipe 1 
-      close(pipefd2[0]);  // Close unused read end of pipe 2
-      close(pipefd2[1]);  // Close unused write end of pipe 2
-      execvp("cat", cat_args); // Execute cat
+
+    int pipefd1[2]; // Pipe between cat and grep
+    int pipefd2[2]; // Pipe between grep and sort
+    int pid1, pid2;
+
+    // Create the first pipe
+    pipe(pipefd1);
+
+    // Fork first child to handle "grep <argument>"
+    pid1 = fork();
+
+    if (pid1 == 0)
+    {
+        // Inside the first child (P2): Execute grep <argument>
+
+        // Create second pipe
+        pipe(pipefd2);
+
+        pid2 = fork();
+        if (pid2 == 0)
+        {
+            // Inside second child (P3): Execute sort
+
+            // Replace standard input with input part of pipe 2
+            dup2(pipefd2[0], 0);
+
+            // Close unused pipe ends
+            close(pipefd2[1]);
+            close(pipefd1[0]);
+            close(pipefd1[1]);
+
+            // Execute sort
+            char *sort_args[] = {"sort", NULL};
+            execvp("sort", sort_args);
+        }
+        else
+        {
+            // Inside first child (P2): Execute grep <argument>
+
+            // Replace standard input with input part of pipe 1
+            dup2(pipefd1[0], 0);
+
+            // Replace standard output with output part of pipe 2
+            dup2(pipefd2[1], 1);
+
+            // Close unused pipe ends
+            close(pipefd1[1]);
+            close(pipefd2[0]);
+            close(pipefd2[1]);
+
+            // Execute grep <argument>
+            char *grep_args[] = {"grep", argv[1], NULL};
+            execvp("grep", grep_args);
+        }
     }
-  }
-  return 0;
+    else
+    {
+        // Inside parent (P1): Execute cat scores
+
+        // Replace standard output with output part of pipe 1
+        dup2(pipefd1[1], 1);
+
+        // Close unused pipe ends
+        close(pipefd1[0]);
+        close(pipefd1[1]);
+
+        // Execute cat scores
+        char *cat_args[] = {"cat", "scores", NULL};
+        execvp("cat", cat_args);
+    }
+
+    return 0;
 }
