44356b4cc5e3e959a34ebb6476e8aa763a5c830b
prighimire <131629703+prighimire@users.noreply.github.com>
2024-12-09 20:08:38 -0500

    initial commit

--- pipes_processes1.c
+++ pipes_processes1.c
@@ -1,81 +1,97 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <sys/wait.h>
-
-int main()
-{
-    int fd1[2];  // Used to store two ends of first pipe
-    int fd2[2];  // Used to store two ends of second pipe
-
-    char fixed_str1[] = "howard.edu";
-    char fixed_str2[] = "gobison.org";
-    char input_str1[100];
-    char input_str2[100];
-    pid_t p;
-
-    if (pipe(fd1) == -1 || pipe(fd2) == -1)
-    {
-        fprintf(stderr, "Pipe Failed");
-        return 1;
-    }
-
-    printf("Enter first string to concatenate: ");
-    scanf("%s", input_str1);
-    p = fork();
-
-    if (p < 0)
-    {
-        fprintf(stderr, "fork Failed");
-        return 1;
-    }
-
-    // Parent process
-    else if (p > 0)
-    {
-        close(fd1[0]);  // Close reading end of first pipe
-
-        // Write input string to child
-        write(fd1[1], input_str1, strlen(input_str1) + 1);
+// C program to demonstrate two-way communication between processes using pipes 
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<string.h> 
+#include<sys/wait.h> 
+  
+int main() 
+{ 
+    int fd1[2];  // Pipe 1 to send input string from parent to child 
+    int fd2[2];  // Pipe 2 to send concatenated string from child to parent 
+  
+    char fixed_str_child[] = "howard.edu"; 
+    char fixed_str_parent[] = "gobison.org"; 
+    char input_str[100]; 
+    char input_str_child[100]; 
+    pid_t p; 
+  
+    if (pipe(fd1) == -1) 
+    { 
+        fprintf(stderr, "Pipe Failed" ); 
+        return 1; 
+    } 
+    if (pipe(fd2) == -1) 
+    { 
+        fprintf(stderr, "Pipe Failed" ); 
+        return 1; 
+    } 
+  
+    printf("Enter a string to concatenate: ");
+    scanf("%s", input_str); 
+  
+    p = fork(); 
+  
+    if (p < 0) 
+    { 
+        fprintf(stderr, "fork Failed" ); 
+        return 1; 
+    } 
+  
+    // Parent process (P1)
+    else if (p > 0) 
+    { 
+        close(fd1[0]);  // Close reading end of first pipe (parent writes)
+        close(fd2[1]);  // Close writing end of second pipe (parent reads)
+  
+        // Write input string to the child and close writing end of first pipe
+        write(fd1[1], input_str, strlen(input_str) + 1); 
         close(fd1[1]);
-
-        // Wait for child to send a string
-        wait(NULL);
-
+  
+        // Wait for child to finish concatenating
+        wait(NULL); 
+  
         // Read concatenated string from child
-        read(fd2[0], input_str2, 100);
-        close(fd2[0]);
+        char concat_str_child[200];
+        read(fd2[0], concat_str_child, 200);
+        printf("Concatenated string from child: %s\n", concat_str_child);
 
-        // Concatenate second fixed string
-        strcat(input_str2, fixed_str2);
-        printf("Final Output: %s\n", input_str2);
-    }
-
-    // Child process
+        // Parent now concatenates "gobison.org" to the string received from child
+        strcat(concat_str_child, fixed_str_parent);
+        printf("Concatenated string with parent suffix: %s\n", concat_str_child);
+  
+        close(fd2[0]);  // Close reading end of second pipe
+    } 
+  
+    // Child process (P2)
     else
-    {
-        close(fd1[1]);  // Close writing end of first pipe
-        
-        // Read string from parent
-        char concat_str[100];
-        read(fd1[0], concat_str, 100);
+    { 
+        close(fd1[1]);  // Close writing end of first pipe (child reads)
+        close(fd2[0]);  // Close reading end of second pipe (child writes)
+  
+        // Read string sent by parent
+        char concat_str[100]; 
+        read(fd1[0], concat_str, 100); 
         close(fd1[0]);
-
-        // Concatenate first fixed string
-        strcat(concat_str, fixed_str1);
-        printf("First Output: %s\n", concat_str);
-
-        printf("Enter second string to concatenate: ");
-        scanf("%s", input_str2);
-
-        // Write second input string to parent
+  
+        // Concatenate "howard.edu"
+        strcat(concat_str, fixed_str_child);
+        printf("Concatenated string in child: %s\n", concat_str);
+  
+        // Send concatenated string back to parent
         write(fd2[1], concat_str, strlen(concat_str) + 1);
+  
+        // Prompt user for another input
+        printf("Enter another string to send to parent: ");
+        scanf("%s", input_str_child);
+
+        // Send this string back to parent (parent will concatenate "gobison.org")
+        write(fd2[1], input_str_child, strlen(input_str_child) + 1);
         close(fd2[1]);
 
         exit(0);
-    }
-
-    return 0;
+    } 
+  
+    return 0; 
 }
